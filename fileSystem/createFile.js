6ba62bdc7499974c2d2b6d7cd719557d91070eec
jignesh sharma <jigneshsharma9868@gmail.com>
2024-01-28 11:30:08 +0530

    learned how to use fs.open()

--- fileSystem/createFile.js
+++ fileSystem/createFile.js
@@ -2,9 +2,32 @@ const fs = require('fs');
 
 // example of fs.appendFile();
 
-fs.appendFile('./append.txt', 'appended text', function(err) {
-    if(err) throw err;
-    console.log("Saved!");
-});
+// fs.appendFile('./append.txt', 'appended text', function(err) {
+//     if(err) throw err;
+//     console.log("Saved!");
+// });
 
+fs.open("./open.txt", 'w', (err, OtherActionOnFile) => {
+       if(err) {
+        console.log("error in opening the file in writing mode",err);
+        return;
+       }
 
+       console.log("file is successfully open for writing:", OtherActionOnFile);
+
+       fs.appendFile(OtherActionOnFile, "this is some content to append", (err) => {
+        if(err) {
+            console.error('error in appending to file:', err);
+            return;
+        }
+        console.log('Content appended successfully');
+        fs.close(OtherActionOnFile, (err) => {
+            if(err) {
+                console.log('error in closing file', err);
+                return;
+            }
+            console.log('file closed successfully');
+        })
+       })
+
+})
